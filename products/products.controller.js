ec45958cbae309f6bb03c2bafdcd31bdbcd0ab14
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-07-08 15:27:36 -0400

    Made site responsive.

--- products/products.controller.js
+++ products/products.controller.js
@@ -5,14 +5,12 @@
     .controller('MainController', function ($scope, ProductService, $routeParams, $location) {
 
       if ($location.url() === '/') {
-        console.log('Got all products.');
         ProductService.getProducts().then(function(products) {
           $scope.products = products;
         });
       }
 
       if($routeParams.productId) {
-        console.log('Got single product.');
         ProductService.getProduct($routeParams.productId).then(function(product) {
           $scope.product = product;
         });
