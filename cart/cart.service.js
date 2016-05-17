ceef8df90049b223465470113a5205ad169cb07e
Clayton Kinder <Clayton@gaugeinteractive.com>
2016-05-17 16:13:20 -0400

    Configuring new server.

--- cart/cart.service.js
+++ cart/cart.service.js
@@ -3,7 +3,7 @@
   angular
     .module('cart')
     .factory('CartService', function ($http, $q, $rootScope) {
-      var url = 'https://samtinyserver.herokuapp.com/collections/krafteekart';
+      var url = 'http://localhost:4730';
 
       var addToCart = function(product){
         $(event.target).children().children().fadeIn(350).fadeOut(350);
